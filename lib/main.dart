806d2074c1effbc7eff63dc60cf11caf0f8fc221
Jacob Petrakovitz <japesc20@gmail.com>
2022-04-27 19:36:35 -0400

    created the background carousel

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'pages/Welcome.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,70 +15,7 @@ class MyApp extends StatelessWidget {
         fontFamily: "GoogleSansRegular",
       ),
       title: 'Jacob Petrakovitz',
-      home: LandingPage(),
-    );
-  }
-}
-
-class LandingPage extends StatelessWidget {
-  const LandingPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          color: Color.fromARGB(255, 65, 65, 65),
-          height: 350.0,
-          alignment: Alignment.topLeft,
-          child: Padding(
-            padding: EdgeInsets.only(top: 80, left: 100),
-            child: Column(
-              children: [
-                Container(
-                  height: 100.0,
-                  width: 100.0,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('assets/code.png'),
-                          fit: BoxFit.fill),
-                      shape: BoxShape.circle),
-                ),
-                Container(
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 40),
-                    child: Text(
-                      'Jacob Petrakovitz',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontFamily: "GoogleSansRegular",
-                          fontStyle: FontStyle.normal,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 25.0,
-                          color: Color.fromARGB(255, 141, 141, 141)),
-                    ),
-                  ),
-                ),
-                Container(
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 20),
-                    child: Text(
-                      'Software Developer',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontFamily: 'Aleo',
-                          fontStyle: FontStyle.normal,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0,
-                          color: Color.fromARGB(255, 141, 141, 141)),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      home: Welcome(),
     );
   }
 }
---
+++ lib/pages/Welcome.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Welcome());
+
+class Welcome extends StatefulWidget {
+  Welcome({Key? key}) : super(key: key);
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  List images = [
+    "welcome-1.png",
+    "welcome-2.png",
+    "welcome-3.jpg",
+    "welcome-4.jpg",
+    "welcome-5.png",
+    "welcome-6.jpg",
+    "welcome-7.png",
+    "welcome-8.jpg",
+    ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: images.length,
+          itemBuilder: (_, index) {
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration:
+                  BoxDecoration(image: DecorationImage(
+                    image: AssetImage(
+                      "assets/images/"+images[index]
+                  ),
+                  fit: BoxFit.fill
+                )
+              ),
+            );
+          }),
+    );
+  }
+}
