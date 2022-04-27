7d775bfeeebd312ccef68d66ca926c9d046d5588
Jacob Petrakovitz <japesc20@gmail.com>
2022-04-27 19:40:59 -0400

    background carousel fix

--- lib/pages/Welcome.dart
+++ lib/pages/Welcome.dart
@@ -19,26 +19,22 @@ class _WelcomeState extends State<Welcome> {
     "welcome-6.jpg",
     "welcome-7.png",
     "welcome-8.jpg",
-    ];
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
+      body: PageView.builder(
           scrollDirection: Axis.vertical,
           itemCount: images.length,
           itemBuilder: (_, index) {
             return Container(
               width: double.maxFinite,
               height: double.maxFinite,
-              decoration:
-                  BoxDecoration(image: DecorationImage(
-                    image: AssetImage(
-                      "assets/images/"+images[index]
-                  ),
-                  fit: BoxFit.fill
-                )
-              ),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/" + images[index]),
+                      fit: BoxFit.cover)),
             );
           }),
     );
